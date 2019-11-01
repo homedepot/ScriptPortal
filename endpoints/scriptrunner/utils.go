f4791f9d92bca32a58ad62da318261da4c807a1a
npmaile <npmaile@gmail.com>
2019-10-31 20:17:45 -0400

    moved types to their own file

--- endpoints/scriptrunner/utils.go
+++ endpoints/scriptrunner/utils.go
@@ -14,32 +14,6 @@ import (
 
 var configuredScripts []script
 
-type script struct {
-	Name           string
-	Interpreter    string
-	FilePath       string
-	Stdin          string
-	FileIn         string
-	Description    string
-	FileExtension  string
-	UUID           string //this is not for the configuration. this is generated as it is sent to the client to keep track of which output stream goes to who.
-	FileUpload     string
-	CmdLineFlags   []flag
-	CmdLineOptions []option
-}
-
-type flag struct {
-	Name        string
-	Prefix      string
-	Description string
-}
-
-type option struct {
-	Name        string
-	Prefix      string
-	Description string
-}
-
 func Init(path string) (err error) {
 	jsonFile, err := os.Open(path)
 	if err != nil {
